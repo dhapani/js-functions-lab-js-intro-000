84392028ce59190a8798e7dd44771b437119b3f9
hilarious-tier-9201 <dhirahapani@hotmail.com>
2020-01-26 03:29:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
 function happyHolidayTo(holiday, name) {
-  return `Happy, ${holiday}, ${name}!`;
+  return `Happy ${holiday}, ${name}!`;
 }
 function holidayCountdown(holiday, days) {
-  return `It's, ${days}, days until, ${holiday}!`;
+  return `It's ${days} days until ${holiday}!`;
 }
 }
